419fa9fd964b193c5eba07f1d84554081db652f3
LuosRestil <metaltasteslikechicken@gmail.com>
2020-05-01 11:49:51 -0500

    ...

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "localhost:5500");
+  next();
+});
+
+app.post("/fetch:country", cors(corsOptions), (req, res) => {
+  fetch(`http://corona-api.com/countries/${req.params.country}`)
+    .then((response) => response.json())
+    .then(res.send(json));
+});
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+const listener = app.listen(port, function () {
+  console.log("Your app is listening on port " + listener.address().port);
+});
