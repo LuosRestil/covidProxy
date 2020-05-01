e5a1c623a31c9be45f9f1f639b0324cbaf3aba93
LuosRestil <metaltasteslikechicken@gmail.com>
2020-05-01 11:58:48 -0500

    ...

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use((req, res, next) => {
 });
 
 app.post("/fetch:country", (req, res) => {
+  console.log("endpoint reached");
   fetch(`http://corona-api.com/countries/${req.params.country}`)
     .then((response) => response.json())
     .then((json) => {
